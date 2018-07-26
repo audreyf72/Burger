265ec55dba49d9e36b75e07367b8522232898b8a
Art Department | Screen Art <artdept@ArtDept-iMac.local>
2018-07-26 16:02:09 -0700

    Initial commit - Added Schema and Seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES('Double Bacon Cheeseburger with Avocado',true),
+      ('Swiss Teriyaki Chicken Burger with Pineapple',true),
+      ('Soy Burger with Sprouts and Tomato',false);
